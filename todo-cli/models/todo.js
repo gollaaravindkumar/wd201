722fb035abe8c2cdd5765f10b5285df9863776cb
gollaaravindkumar <gollaaravindkumar143@gmail.com>
2024-09-15 18:17:40 +0530

    Update todo-cli

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -2,63 +2,52 @@
 'use strict';
 const { Model, Op } = require('sequelize');
 const moment = require('moment');
+
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static async addTask(params) {
       return await Todo.create(params);
     }
+
     static async showList() {
       console.log('My Todo list \n');
 
       console.log('Overdue');
-      // FILL IN HERE
       const overdueTasks = await Todo.overdue();
       overdueTasks.forEach(task => console.log(task.displayableString()));
       console.log('\n');
 
       console.log('Due Today');
-      // FILL IN HERE
       const todayTasks = await Todo.dueToday();
       todayTasks.forEach(task => console.log(task.displayableString()));
       console.log('\n');
 
       console.log('Due Later');
-      // FILL IN HERE
       const laterTasks = await Todo.dueLater();
       laterTasks.forEach(task => console.log(task.displayableString()));
       console.log('\n');
     }
 
     static async overdue() {
-      // FILL IN HERE TO RETURN OVERDUE ITEMS
       const today = moment().format('YYYY-MM-DD');
       return await Todo.findAll({
         where: {
           dueDate: {
             [Op.lt]: today,
           },
-          completed: false,
         },
       });
     }
-
     static async dueToday() {
-      // FILL IN HERE TO RETURN ITEMS DUE tODAY\
       const today = moment().format('YYYY-MM-DD');
       return await Todo.findAll({
         where: {
           dueDate: today,
-          },
+        },
       });
     }
 
     static async dueLater() {
-      // FILL IN HERE TO RETURN ITEMS DUE LATER
       const today = moment().format('YYYY-MM-DD');
       return await Todo.findAll({
         where: {
@@ -70,9 +59,8 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     static async markAsComplete(id) {
-      // FILL IN HERE TO MARK AN ITEM AS COMPLETE
       const task = await Todo.findByPk(id);
-      if(task) {
+      if (task) {
         task.completed = true;
         await task.save();
       }
@@ -81,9 +69,10 @@ module.exports = (sequelize, DataTypes) => {
     displayableString() {
       let checkbox = this.completed ? '[x]' : '[ ]';
       let dateDisplay = this.dueDate === moment().format('YYYY-MM-DD') ? '' : this.dueDate;
-      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
+      return `${this.id}. ${checkbox} ${this.title} ${dateDisplay}`.trim();
     }
   }
+
   Todo.init(
     {
       title: DataTypes.STRING,
@@ -93,7 +82,8 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: 'Todo',
-    },
+    }
   );
+
   return Todo;
 };
